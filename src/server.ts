f03ce827f82a31f3b7e7aa535af67fb167b9cfd6
MatheusPMC <matheus.pmc@hotmail.com>
2021-06-23 20:32:13 -0300

    database

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
-import express, { request, response } from "express";
+import "reflect-metadata";
+import express from "express";
+
+import "./database";
 
 const app = express();
-
-app.get("/test", (request, response) => {
-  response.send("Olá Mundo")
-});
-
-app.post("/test-post", (request, response) => {
-  return response.send("Olá Mundo metodo Post")
-})
 
 app.listen(3000, () => console.log("Server is running"));
