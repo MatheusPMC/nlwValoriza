2fbeb309eb2df4248ecca3a1bcc243cc12d15eee
MatheusPMC <matheus.pmc@hotmail.com>
2021-06-26 14:14:16 -0300

    created Compliment

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -27,12 +27,12 @@ class AuthenticateUserService {
 
     const token = sign(
       { email: user.email },
-      "26ea83fd842c68ba5eed0a9e40d54f5f". {
+      "26ea83fd842c68ba5eed0a9e40d54f5f", {
       subject: user.id,
       expiresIn: "1d"
     });
 
-    return token;
+    return token
   }
 
 }
