7b807fa98e4b5545b17ca2ace6151900270c3ac6
MatheusPMC <matheus.pmc@hotmail.com>
2021-06-26 12:57:45 -0300

    Authenticate

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository } from "typeorm"
+import { compare } from "bcryptjs";
+import { UsersRepositories } from "../repositories/UsersRepositories"
+import { sign } from "jsonwebtoken";
+
+interface iAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+
+  async execute({ email, password }: iAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories)
+
+    const user = await usersRepositories.findOne({ email });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect")
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect")
+    }
+
+    const token = sign(
+      { email: user.email },
+      "26ea83fd842c68ba5eed0a9e40d54f5f". {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    return token;
+  }
+
+}
+
+export { AuthenticateUserService }
